9d3439c34f6ed8b8788ea2343e47eb711aabde6b
AngelCalle <angelcalleclase@gmail.com>
2022-01-10 21:17:31 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-ngx-skeleton-loader';
+  
+  contentLoaded: boolean = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.contentLoaded = true;
+    }, 2000);
+  }
 }
